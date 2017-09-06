7fe77798dd834bef917617bce746481559735bd5
PC <PC@DESKTOP-613>
2017-09-06 13:19:18 +0200

    Intento de tarjetas

---
+++ pizzeria/Scripts/CustomScripts/CustomScript2.js
@@ -0,0 +1,11 @@
+﻿angular.module('Pizzeria', ['ngMaterial'])
+
+    .controller('card', function ($scope) {
+        $scope.imagePath = '~/Views/Pizzas/img/Campina.jpg';
+    })
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+        $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+        $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+        $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+    });
