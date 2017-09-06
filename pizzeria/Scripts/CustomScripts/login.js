49370be9a1bb4a9f91009de6e91cf2188d80599c
PC <PC@DESKTOP-613>
2017-09-06 10:14:53 +0200

    Agregar archivos de proyecto.

---
+++ pizzeria/Scripts/CustomScripts/login.js
@@ -0,0 +1,4 @@
+﻿var app = angular.module('Pizzeria', ['ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages'])
+    .controller('CtrlLogin', function ($scope) {
+        $scope.formData = {};
+    });
---
+++ pizzeria/Scripts/CustomScripts/register.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module('Pizzeria', ['ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages'])
+    .controller('CtrlRegister', function ($scope) {
+        $scope.formData = {};
+        $scope.passwordStrength = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,}$/;
+    });
