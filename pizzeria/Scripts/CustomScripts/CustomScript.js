19c8aa6b403f2f5574e6662109fc6550d4d25c11
PC <PC@DESKTOP-613>
2017-09-06 12:23:57 +0200

    Boton sidenav funcionando. Ajuste layout

---
+++ pizzeria/Scripts/CustomScripts/CustomScript.js
@@ -0,0 +1,12 @@
+﻿angular
+    .module('Pizzeria', ['ngMaterial'])
+    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
+        $scope.toggleLeft = buildToggler('left');
+        $scope.toggleRight = buildToggler('right');
+
+        function buildToggler(componentId) {
+            return function () {
+                $mdSidenav(componentId).toggle();
+            };
+        }
+    });
