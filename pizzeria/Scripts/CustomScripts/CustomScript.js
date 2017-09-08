a3b704941dda853ba01364c53b2ee238d8af711a
Guillermo Sáez Sánchez <guiyersaez@hotmail.com>
2017-09-08 10:35:09 +0200

    Actualizar

--- pizzeria/Scripts/CustomScripts/CustomScript.js
+++ pizzeria/Scripts/CustomScripts/CustomScript.js
@@ -1,5 +1,5 @@
-﻿angular
-    .module('Pizzeria', ['ngMaterial'])
+﻿var pizzaApp = angular
+    .module('Pizzeria', ['ngMaterial', 'ngRoute'])
     .controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
         $scope.toggleLeft = buildToggler('left');
         $scope.toggleRight = buildToggler('right');
@@ -10,3 +10,30 @@
             };
         }
     });
+
+pizzaApp.config(function ($routeProvider){
+    $routeProvider
+        .when('/', {
+            template:'<h1>Index</h1>'
+        })
+        .when('/pizzas',{
+            templateUrl:'Content/pizzas/listPizzas.html'
+        })
+        .when('/addPizzas', {
+            templateUrl: 'Content/pizzas/addPizzas.html'
+        })
+        .otherwise({
+            template:'<h1>Pagina no existente</h1>'
+        })
+});
+
+pizzaApp.controller('CardCtrl', function ($scope) {
+    $scope.imagePath = 'img/washedout.png';
+});
+
+pizzaApp.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+});
